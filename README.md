af1a590625ba75cbae27b137cc8731bcfc6de973
azhardotme <azharcse1997@gmail.com>
2022-10-25 20:22:27 +0600

    feature/vendors crud & pagination

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # library-management-system
+# এসো বই পড়ি
+
+### Feature/Vendors
+ -  Retrieve and Render All Vendors
+ -  Add vendors
+ -  Edit vendors data
+ -  Update vendor data
+ -  Delete vendor
+ -  Pagination
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\VendorsController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('index');
 });
+
+# Vendors (Azhar)
+Route::get('vendors', [VendorsController::class, 'index'])->name('vendors.index');
+Route::get('vendors/create', [VendorsController::class, 'create'])->name('vendors.create');
+Route::post('/vendors', [VendorsController::class, 'store'])->name('vendors.store');
+Route::get('vendors/{id}/edit', [VendorsController::class, 'edit'])->name('vendors.edit');
+Route::put('/vendors/{id}', [VendorsController::class, 'update'])->name('vendors.update');
+Route::delete('/vendors/{id}', [VendorsController::class, 'destroy'])->name('vendors.destroy');
