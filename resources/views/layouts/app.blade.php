7a25fc071df68cd6059e3d89e613e9148f2bee6f
abi30 <rakib0751@gmail.com>
2022-10-27 00:12:36 +0200

    work at home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,80 @@
-<!DOCTYPE html>
-<html>
-
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Custom Auth in Laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-
 <body>
-    <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
-        <div class="container">
-            <a class="navbar-brand mr-auto" href="#">library-management-system</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register-user') }}">Register</a>
-                    </li>
-                    @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-    @yield('content')
-</body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,30 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\HomeController;
 
 
 
-// Route::get('/', function () {
-//     return view('index');
-// });
-
-
-
-Route::middleware(['guest'])->group(function () {
-
-    Route::get('/', [AuthController::class, 'index']);
-    Route::get('/login', [AuthController::class, 'index'])->name('login');
-    Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
-    Route::get('registration', [AuthController::class, 'registration'])->name('register-user');
-    Route::post('custom-registration', [AuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('/', function () {
+    return view('welcome');
 });
 
 
+
+// Route::middleware(['guest'])->group(function () {
+
+//     Route::get('/', [AuthController::class, 'index']);
+//     Route::get('/login', [AuthController::class, 'index'])->name('login');
+//     Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
+//     Route::get('registration', [AuthController::class, 'registration'])->name('register-user');
+//     Route::post('custom-registration', [AuthController::class, 'customRegistration'])->name('register.custom');
+// });
+
+
 Route::get('signout', [AuthController::class, 'signOut'])->name('signout');
-
-
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [AuthController::class, 'home'])->name('home');
@@ -44,9 +44,7 @@ Route::middleware(['auth', 'isadmin'])->group(function () {
 });
 
 
+Auth::routes();
 
-// Route::get('/authors', [AuthorsController::class, 'index'])->name('authors.index');
-// //show add author view
-// Route::get('admin/authors/create', [AuthorsController::class, 'create'])->name('manage.authors.create');
-// //store new author
-// Route::post('/authors', [AuthorsController::class, 'store'])->name('manage.authors.store');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
