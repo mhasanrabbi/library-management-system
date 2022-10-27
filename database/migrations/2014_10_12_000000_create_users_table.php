bdc2789a178ef60e425870063addcf9d0d5745b8
abi30 <rakib0751@gmail.com>
2022-10-27 13:25:49 +0200

    test and test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address')->nullable();
-            $table->integer('role')->default(0);
-            $table->string('user_status')->default('active');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique()->nullable();
             $table->string('password');
+            $table->string('user_image')->nullable();
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedTinyInteger('role')->default(0);
+            $table->boolean('receive_email')->default(true);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
