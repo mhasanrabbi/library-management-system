ade74a3e058f481c0c4354408bd756b26a895caa
abi30 <rakib0751@gmail.com>
2022-11-03 17:20:16 +0100

    last add .

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <br>
     <div class="float-right">
-        <a class="btn btn-success" href="{{ action([CartController::class,'index']) }}">
+        <a class="btn btn-success" href="{{ action([CartController::class,'index'])}}">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart"
                 viewBox="0 0 16 16">
                 <path
